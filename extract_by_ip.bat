612463b17973a4db2928f9b8cf19bada49bf228d
xuwenbo <xuwenbo_outlook@outlook.com>
2017-07-29 16:26:54 +0800

    dns, http info

--- extract_by_ip.bat
+++ extract_by_ip.bat
@@ -1,14 +1,31 @@
 @echo off
 
-::ip.proto = 1,2,6,17  as icmp, igmp, tcp, udp
-echo "---------------------info about udp-------------------"
-tshark -2 -r %1 -R ip.proto==17 -T fields -e ip.src -e udp.srcport -e ip.dst -e udp.dstport | sort | uniq
+cd %~dp1
 
-echo "---------------------info about tcp--------------------"
-tshark -2 -r %1 -R ip.proto==6 -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport | sort | uniq
+echo "---------------------info about mac------------------" >> %~dp1log_tcp_udp.txt 2>&1
+tshark -r %1 -T fields -e eth.addr | sort | uniq >> %~dp1log_tcp_udp.txt
+
+echo "---------------------info about mac interaction------" >> %~dp1log_tcp_udp.txt 2>&1
+tshark -r %1 -T fields -e eth.dst -e ip.dst | sort | uniq >> %~dp1log_tcp_udp.txt
+
+echo "---------------------info about who is dns querying--" >> %~dp1log_tcp_udp.txt 2>&1
+tshark -2 -r %1 -R udp.dstport==53 -T fields -e ip.src -e dns.qry.name | sort | uniq >> %~dp1log_tcp_udp.txt 
+
+echo "---------------------info about who is http requesting--" >> %~dp1log_tcp_udp.txt 2>&1
+tshark -2 -r %1 -R tcp.dstport==80 -T fields -e ip.src -e http.request.full_uri | sort | uniq >> %~dp1log_tcp_udp.txt
+
+echo "---------------------info about mail address------------" >> %~dp1log_tcp_udp.txt 2>&1
+tshark -2 -r %1 -R "data-text-lines" -T fields -e text | grep -Eio '\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b' | sort | uniq >> %~dp1log_tcp_udp.txt
+
+::ip.proto = 1,2,6,17  as icmp, igmp, tcp, udp
+echo "---------------------info about udp-------------------" >> %~dp1log_tcp_udp.txt 2>&1
+tshark -2 -r %1 -R ip.proto==17 -T fields -e ip.src -e udp.srcport -e ip.dst -e udp.dstport | sort | uniq >> %~dp1log_tcp_udp.txt 2>&1
+
+echo "---------------------info about tcp--------------------" >> %~dp1log_tcp_udp.txt 2>&1
+tshark -2 -r %1 -R ip.proto==6 -T fields -e ip.src -e tcp.srcport -e ip.dst -e tcp.dstport | sort | uniq >> %~dp1log_tcp_udp.txt 2>&1
 
 echo "---------------------info about all ip----------------"
-tshark -r %1 -T fields -e ip.dst | sort | uniq > stat_all_ip.txt
+tshark -r %1 -T fields -e ip.dst | sort | uniq > %~dp1stat_all_ip.txt
 
-mkdir extract
-for /F "tokens=*" %%A in (stat_all_ip.txt) do tshark -2 -r %1 -w extract\%%A.pcap -Y "ip.dst==%%A or ip.src==%%A"
+::mkdir extract
+::for /F "tokens=*" %%A in (stat_all_ip.txt) do tshark -2 -r %1 -w extract\%%A.pcap -Y "ip.dst==%%A or ip.src==%%A"
